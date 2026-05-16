8e2eb5b3656134dabcca09d8c12554a4cf2e909a
vitoriaduran <gabyvitoria171205@gmail.com>
2026-05-16 09:40:51 -0300

    ajustando porta dinamica

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,11 @@
 from app import create_app, db
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    
+    porta = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=porta)
